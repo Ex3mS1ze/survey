587e3cc726e4620f2ccdb7ff86e9dbeaa8679d86
Arkady Belkov <ex3msize@gmail.com>
2020-02-12 14:20:47 +0700

    Add JDBC spring session On rememberMe

--- src/main/java/com/survey/config/WebSecurityConfig.java
+++ src/main/java/com/survey/config/WebSecurityConfig.java
@@ -36,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout()
                     .permitAll()
-                    .logoutSuccessUrl("/");
+                    .logoutSuccessUrl("/")
+                .and()
+                    .rememberMe().tokenValiditySeconds(86400);
     }
 }
